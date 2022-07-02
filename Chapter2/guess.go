7189430d06ccf5a17057b236db1a31720bb7407b
Jann <l33klin@foxmail.com>
2022-07-02 21:39:11 +0800

    add exercise code

---
+++ Chapter2/guess.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	target := rand.Intn(100) + 1
+	chances := 10
+	for chances > 0 {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Please input a number(should less than 100): ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		guessNum, err := strconv.Atoi(strings.TrimSpace(input))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guessNum > target {
+			fmt.Println("The number your input is too big!")
+		} else if guessNum < target {
+			fmt.Println("The number your input is too little!")
+		} else {
+			fmt.Println("Cool! You got the number!")
+			break
+		}
+		chances--
+		if chances <= 0 {
+			fmt.Printf("You've out of chance, the target is: %v.\n", target)
+			break
+		}
+	}
+}
