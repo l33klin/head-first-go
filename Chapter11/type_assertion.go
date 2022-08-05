02f849a75a075256f2929d7a54cd3a7f4a94743d
Jann <l33klin@foxmail.com>
2022-08-05 23:19:37 +0800

    type assertion

---
+++ Chapter11/type_assertion.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type Player interface {
+	Play(string)
+	Stop()
+}
+
+type TapePlayer string
+
+// TapePlayer type definition here
+func (t TapePlayer) Play(song string) {
+	fmt.Println("Playing", song)
+}
+func (t TapePlayer) Stop() {
+	fmt.Println("Stopped!")
+}
+
+type TapeRecorder string
+
+// TapeRecorder type definition here
+func (t TapeRecorder) Play(song string) {
+	fmt.Println("Playing", song)
+}
+func (t TapeRecorder) Record() {
+	fmt.Println("Recording")
+}
+func (t TapeRecorder) Stop() {
+	fmt.Println("Stopped!")
+}
+
+func TryOut(player Player) {
+	player.Play("Song~")
+	player.Stop()
+	if tr, ok := player.(TapeRecorder); ok {
+		tr.Record()
+	}
+}
+
+func main() {
+	tr := TapeRecorder("t")
+	TryOut(tr)
+	tp := TapePlayer("t")
+	TryOut(tp)
+}
