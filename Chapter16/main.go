6b13ca6994b68f4d650f7130d2708f93ff48abb7
Jann <l33klin@foxmail.com>
2022-12-04 17:58:49 +0800

    add chapter16 complete

--- Chapter16/main.go
+++ Chapter16/main.go
@@ -13,6 +13,8 @@ func check(err error) {
 
 func main() {
 	http.HandleFunc("/guestbook", guestBookHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
--- Chapter16/view.go
+++ Chapter16/view.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"html/template"
 	"net/http"
 	"os"
-	"text/template"
 )
 
 type Guestbook struct {
@@ -22,6 +23,27 @@ func guestBookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	err = html.Execute(w, guestbook)
 	check(err)
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	err = html.Execute(w, nil)
+	check(err)
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	signature := r.FormValue("signature")
+	if signature != "" {
+		// write to file
+		options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+		file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+		check(err)
+		_, err = fmt.Fprintln(file, signature)
+		check(err)
+		err = file.Close()
+		check(err)
+	}
+	http.Redirect(w, r, "/guestbook", http.StatusFound)
 }
 
 func getStrings(fileName string) []string {
